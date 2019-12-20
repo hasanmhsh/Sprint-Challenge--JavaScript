8d9b04e2fac0e8a2a78c6b4ee59e7d44c9091d6c
Hasan M.Shehata <hasan.mh.sh@gmail.com>
2019-12-20 08:58:41 +0200

    Arrays Request 1

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -55,7 +55,7 @@ const graduates = [
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array. This will be an array of strings.
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
-const universities = [];
+const universities = graduates.map((item)=>item.university).sort();
 console.log(universities);
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
