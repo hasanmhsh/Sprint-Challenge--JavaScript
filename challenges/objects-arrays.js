c662e1f0fbd990afc95b3f211f251f0cb2c731aa
Hasan M.Shehata <hasan.mh.sh@gmail.com>
2019-12-20 09:11:14 +0200

    Arrays Request 3

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -68,7 +68,12 @@ const contactInfo = graduates.map((item)=>`${item.first_name} ${item.email}`);
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
-const unisWithUni = [];
+const unisWithUni = graduates.filter((item)=>{
+  if(item.university.includes("Uni")){
+    return true;
+  }
+  return false;
+}).map((item)=>{return {university:`${item.university}`};});
 console.log(unisWithUni);
 
 
